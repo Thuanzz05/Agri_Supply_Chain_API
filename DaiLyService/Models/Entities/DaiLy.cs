58d5e5d6b18a07a5e43fa95c2e1d9181e3932fe0
Thuanzz05 <thuantoppo@gmail.com>
2025-12-10 11:21:49 +0700

    Up Models từng services

---
+++ DaiLyService/Models/Entities/DaiLy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DaiLyService.Models.Entities;
+
+public partial class DaiLy
+{
+    public int MaDaiLy { get; set; }
+
+    public int MaTaiKhoan { get; set; }
+
+    public string? TenDaiLy { get; set; }
+
+    public string? SoDienThoai { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? DiaChi { get; set; }
+
+    public virtual ICollection<DonHangDaiLy> DonHangDaiLies { get; set; } = new List<DonHangDaiLy>();
+
+    public virtual ICollection<Kho> Khos { get; set; } = new List<Kho>();
+
+    public virtual ICollection<KiemDinh> KiemDinhs { get; set; } = new List<KiemDinh>();
+
+    public virtual TaiKhoan MaTaiKhoanNavigation { get; set; } = null!;
+}
