d22e6c317a654b4f1e8c6910506cd90b3b197cec
hi <truongdxyz6@gmail.com>
2025-12-19 15:37:12 +0700

    up DTOs SanPham

---
+++ NongDanService/Models/DTOs/SanPhamDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace NongDanService.Models.DTOs
+{
+    public class SanPhamDTO
+    {
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; } = null!;
+        public string DonViTinh { get; set; } = null!;
+        public string? MoTa { get; set; }
+    }
+}
