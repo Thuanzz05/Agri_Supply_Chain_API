dfb22a7620cf8253547c8ed4d96104d9835b139b
hi <truongdxyz6@gmail.com>
2025-12-26 15:31:21 +0700

    Sua Service SanPham

--- NongDanService/Services/SanPhamService.cs
+++ NongDanService/Services/SanPhamService.cs
@@ -1,6 +1,5 @@
 ﻿using NongDanService.Data;
 using NongDanService.Models.DTOs;
-using NongDanService.Models.Entities;
 
 namespace NongDanService.Services
 {
@@ -13,67 +12,15 @@ namespace NongDanService.Services
             _repo = repo;
         }
 
-        public async Task<List<SanPhamDTO>> GetAll()
-        {
-            var list = await _repo.GetAll();
-            return list.Select(x => new SanPhamDTO
-            {
-                MaSanPham = x.MaSanPham,
-                TenSanPham = x.TenSanPham,
-                DonViTinh = x.DonViTinh,
-                MoTa = x.MoTa
-            }).ToList();
-        }
+        public List<SanPhamDTO> GetAll() => _repo.GetAll();
 
-        public async Task<SanPhamDTO?> GetById(int id)
-        {
-            var sp = await _repo.GetById(id);
-            if (sp == null) return null;
+        public SanPhamDTO? GetById(int id) => _repo.GetById(id);
 
-            return new SanPhamDTO
-            {
-                MaSanPham = sp.MaSanPham,
-                TenSanPham = sp.TenSanPham,
-                DonViTinh = sp.DonViTinh,
-                MoTa = sp.MoTa
-            };
-        }
+        public int Create(SanPhamCreateDTO dto) => _repo.Create(dto);
 
-        public async Task<int> Create(SanPhamCreateDTO dto)
-        {
-            var sp = new SanPham
-            {
-                TenSanPham = dto.TenSanPham,
-                DonViTinh = dto.DonViTinh,
-                MoTa = dto.MoTa,
-                NgayTao = DateTime.Now
-            };
+        public bool Update(int id, SanPhamUpdateDTO dto)
+            => _repo.Update(id, dto);
 
-            await _repo.Create(sp);
-            return sp.MaSanPham;
-        }
-
-        public async Task<bool> Update(int id, SanPhamUpdateDTO dto)
-        {
-            var sp = await _repo.GetById(id);
-            if (sp == null) return false;
-
-            sp.TenSanPham = dto.TenSanPham;
-            sp.DonViTinh = dto.DonViTinh;
-            sp.MoTa = dto.MoTa;
-
-            await _repo.Update(sp);
-            return true;
-        }
-
-        public async Task<bool> Delete(int id)
-        {
-            var sp = await _repo.GetById(id);
-            if (sp == null) return false;
-
-            await _repo.Delete(sp);
-            return true;
-        }
+        public bool Delete(int id) => _repo.Delete(id);
     }
-    
 }
